38d9667b4cf5fb8bce1592c403bd6da2c4558c1c
xaro <xabilro@gmail.com>
2021-09-27 15:03:50 +0200

    remove example component references

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -1,7 +1,6 @@
 import {TestBed} from '@angular/core/testing';
 import {RouterTestingModule} from '@angular/router/testing';
 import {AppComponent} from './app.component';
-import {ExampleComponent} from "./example-component/example.component";
 import {HttpClientModule} from "@angular/common/http";
 
 describe('AppComponent', () => {
@@ -12,8 +11,7 @@ describe('AppComponent', () => {
         HttpClientModule
       ],
       declarations: [
-        AppComponent,
-        ExampleComponent
+        AppComponent
       ],
     }).compileComponents();
   });
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,14 +3,12 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import {ExampleComponent} from './example-component/example.component';
 import {HttpClientModule} from "@angular/common/http";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ExampleComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
