62e880ebbef25f9f7b9092c0785fed4f67b1c043
xaro <xabilro@gmail.com>
2021-09-27 17:20:25 +0200

    add skills adding functionality

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -14,6 +14,7 @@ import {MatInputModule} from "@angular/material/input";
 import {MatButtonModule} from "@angular/material/button";
 import {MatIconModule} from "@angular/material/icon";
 import {MatTableModule} from "@angular/material/table";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -32,7 +33,8 @@ import {MatTableModule} from "@angular/material/table";
     MatInputModule,
     MatButtonModule,
     MatIconModule,
-    MatTableModule
+    MatTableModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/skills/skills.component.ts
+++ frontend/src/app/skills/skills.component.ts
@@ -1,4 +1,5 @@
-import {Component} from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {MatTable} from "@angular/material/table";
 
 @Component({
   selector: 'app-skills',
@@ -8,6 +9,15 @@ import {Component} from '@angular/core';
 export class SkillsComponent {
   skills: Skill[] = [{name: "Spring"}, {name: "CSS"}, {name: "HTML"}];
   displayedColumns: string[] = ["name"];
+  skillNameInput: string = "";
+
+  @ViewChild(MatTable) table!: MatTable<Skill>;
+
+  addSkill(skillName: string): void {
+    this.skills.push({name: skillName});
+    this.skillNameInput = "";
+    this.table.renderRows();
+  }
 }
 
 export interface Skill {
