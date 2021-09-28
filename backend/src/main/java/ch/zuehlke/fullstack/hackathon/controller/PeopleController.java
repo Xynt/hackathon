e65c1226d3b301359dfebb59c61dc1c262c47d9c
Lyndsey Bonelli <lyndsey.bonelli@zuehlke.com>
2021-09-28 20:57:55 +0200

    Add CrossOrigin annotations.

--- backend/src/main/java/ch/zuehlke/fullstack/hackathon/controller/PeopleController.java
+++ backend/src/main/java/ch/zuehlke/fullstack/hackathon/controller/PeopleController.java
@@ -5,6 +5,7 @@ import com.zuehlke.hackathon.peoplefinder.api.PeopleApi;
 import com.zuehlke.hackathon.peoplefinder.model.Person;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 
@@ -16,6 +17,7 @@ public class PeopleController implements PeopleApi {
         this.peopleService = peopleService;
     }
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @Override
     public ResponseEntity<List<Person>> getPeople() {
         List<Person> people = this.peopleService.getPeople();
