beab2d2d9e5b581092a40bcb039d6a73e0ac68c5
Lyndsey Bonelli <lyndsey.bonelli@zuehlke.com>
2021-09-29 02:12:53 +0200

    Use the new team setup service to store the configurations centrally.

--- frontend/src/app/persons/persons.component.ts
+++ frontend/src/app/persons/persons.component.ts
@@ -9,6 +9,7 @@ import {Skill} from "../../../peoplefinder-api/model/skill";
 import {map, startWith} from "rxjs/operators";
 import {MatTable} from "@angular/material/table";
 import {PeopleServiceApi} from "../../../peoplefinder-api/api/people.service";
+import {TeamSetupService} from "../service/team-setup.service";
 
 @Component({
   selector: 'app-persons',
@@ -16,39 +17,34 @@ import {PeopleServiceApi} from "../../../peoplefinder-api/api/people.service";
   styleUrls: ['./persons.component.scss']
 })
 export class PersonsComponent implements OnInit {
+  selectedPeople: Person[] = [];
+  selectedSkills: Skill[] = [];
 
-  persons: Person[] = [];
+  selectablePeople: Person[] = [];
+
   displayedColumns: string[] = ["code", "firstname", "lastname"];
-
   filteredSuggestions!: Observable<Person[]>;
-  allPersons: Person[] = [];
 
-  notExistsValidator: ValidatorFn = notExistsValidator(this.persons.map(person => person.code));
-
+  notExistsValidator: ValidatorFn = notExistsValidator(this.selectedPeople.map(person => person.code));
   personControl: FormControl = new FormControl("", []);
-  skills: Skill[] = [];
 
   @ViewChild(MatTable) table!: MatTable<Skill>;
 
-  constructor(private router: Router, private peopleServiceApi: PeopleServiceApi) {
-    try {
-      this.skills = router.getCurrentNavigation()!.extras.state!.selectedSkills;
-    } catch (e) {
-      this.router.navigate(["/skills"]);
-    }
-
-    try {
-      this.persons = router.getCurrentNavigation()!.extras.state!.selectedPersons;
-    } catch (e) {
-    }
+  constructor(private router: Router, private peopleServiceApi: PeopleServiceApi, private teamSetupService: TeamSetupService) {
   }
 
   ngOnInit(): void {
+    this.selectedSkills = [...this.teamSetupService.skills];
+    if (!this.selectedSkills) {
+      this.router.navigate(["/skills"]);
+    }
+    this.selectedPeople = [...this.teamSetupService.people];
+
     this.peopleServiceApi.getPeople().subscribe(people => {
-      this.allPersons = people;
+      this.selectablePeople = people;
       this.personControl.addValidators([
         Validators.required,
-        existsValidator(this.allPersons.map(person => person.code)),
+        existsValidator(this.selectablePeople.map(person => person.code)),
         this.notExistsValidator
       ])
     })
@@ -62,7 +58,7 @@ export class PersonsComponent implements OnInit {
 
   addPerson(): void {
     if (this.personControl.valid) {
-      this.persons.push(this.allPersons.find(person => person.code == this.personControl.value)!);
+      this.selectedPeople.push(this.selectablePeople.find(person => person.code == this.personControl.value)!);
       this.resetExistsValidator()
       this.table.renderRows();
       this.personControl.reset();
@@ -71,12 +67,17 @@ export class PersonsComponent implements OnInit {
 
   resetExistsValidator(): void {
     this.personControl.removeValidators(this.notExistsValidator)
-    this.notExistsValidator = notExistsValidator(this.persons.map(person => person.code));
+    this.notExistsValidator = notExistsValidator(this.selectedPeople.map(person => person.code));
     this.personControl.addValidators(this.notExistsValidator);
   }
 
   navigateBack(): void {
-    this.router.navigate(["skills"], {state: {selectedSkills: this.skills, selectedPersons: this.persons}})
+    this.router.navigate(["skills"])
+  }
+
+  continueWithData(): void {
+    this.teamSetupService.people = this.selectedPeople;
+    this.router.navigate(["config"])
   }
 
   _filter(value: string): Person[] {
@@ -86,7 +87,7 @@ export class PersonsComponent implements OnInit {
 
     const filterValue = value.toLowerCase();
 
-    let existingValuesRemovedSuggestions = this.allPersons.filter(option => !this.persons.map(person => person.code).includes(option.code))
+    let existingValuesRemovedSuggestions = this.selectablePeople.filter(option => !this.selectedPeople.map(person => person.code).includes(option.code))
     let searchFilteredSuggestions = existingValuesRemovedSuggestions.filter(option => {
       let includesCode = option.code.toLowerCase().includes(filterValue);
       let includesName = (option.firstName.toLowerCase() + " " + option.lastName.toLowerCase()).includes(filterValue);
