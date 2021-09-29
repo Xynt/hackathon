e07ba1591d4e9397077cfc18b092f2c855483490
Lyndsey Bonelli <lyndsey.bonelli@zuehlke.com>
2021-09-29 03:27:03 +0200

    Add toolbar.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,11 +4,11 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {HttpClientModule} from "@angular/common/http";
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { SkillsComponent } from './skills/skills.component';
-import { PersonsComponent } from './persons/persons.component';
-import { ConfigComponent } from './config/config.component';
-import { ResultComponent } from './result/result.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {SkillsComponent} from './skills/skills.component';
+import {PersonsComponent} from './persons/persons.component';
+import {ConfigComponent} from './config/config.component';
+import {ResultComponent} from './result/result.component';
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatInputModule} from "@angular/material/input";
 import {MatButtonModule} from "@angular/material/button";
@@ -16,9 +16,10 @@ import {MatIconModule} from "@angular/material/icon";
 import {MatTableModule} from "@angular/material/table";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {MatAutocompleteModule} from "@angular/material/autocomplete";
-import { ExistsDirective } from './service/directive/exists.directive';
-import { NotExistsDirective } from './service/directive/not-exists.directive';
-import { PageNavigatorButtonsComponent } from './page-navigator-buttons/page-navigator-buttons.component';
+import {ExistsDirective} from './service/directive/exists.directive';
+import {NotExistsDirective} from './service/directive/not-exists.directive';
+import {PageNavigatorButtonsComponent} from './page-navigator-buttons/page-navigator-buttons.component';
+import {MatToolbarModule} from "@angular/material/toolbar";
 
 @NgModule({
   declarations: [
@@ -41,6 +42,7 @@ import { PageNavigatorButtonsComponent } from './page-navigator-buttons/page-nav
     MatButtonModule,
     MatIconModule,
     MatTableModule,
+    MatToolbarModule,
     FormsModule,
     MatAutocompleteModule,
     ReactiveFormsModule
